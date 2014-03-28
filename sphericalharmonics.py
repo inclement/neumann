e6aea6ba71523ae19a7abaec7c1dcf6b899e4f45
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-03-28 13:05:37 +0000

    Spherical harmonics improvements

--- sphericalharmonics.py
+++ sphericalharmonics.py
@@ -79,6 +79,11 @@ class NeumannCubeHandler(object):
                                        partial(self.cell_side_function, i),
                                        face=i, handler=self))
 
+    def find_critical_points(self):
+        '''Ask each tracer in self.tracers to find its critical points.'''
+        for tracer in self.tracers:
+            tracer.find_critical_points()
+
     def trace_lines(self):
         '''Ask each tracer in self.tracers to find its Neumann lines. Ignores
         boundaries for the time being.'''
@@ -94,18 +99,81 @@ class NeumannCubeHandler(object):
         theta, phi = cube_to_angles(*position)
         return self.func(theta, phi)
 
-    def plot_stereographic(self):
+    def plot_3d(self, stereographic=True, plot_criticals=False, trace_lines=False,
+                clf=True):
         '''Plot self and lines via stereographic projection.'''
-        pass
+        if clf:
+            may.clf()
+        if plot_criticals:
+            self.find_critical_points()
+        if trace_lines:
+            self.trace_lines()
 
-    def plot_net(self, trace_lines=False):
+        arrs = [n.rot90(tracer.arr[::-1], 3)*-1 for tracer in self.tracers]
+
+        arr_max = n.max([n.max(n.abs(arr)) for arr in arrs])
+        
+        shape = self.shape
+
+        if stereographic:
+            for side, arr in enumerate(arrs):
+                arr[0, 0] = arr_max
+                arr[0, 1] = -1*arr_max
+                if side != 1:
+                    xs, ys, zs = side_to_xs_ys_zs(side, shape)
+                    stxs, stys = vcube_to_stereographic_projection(xs, ys, zs)
+                    real_zs = n.zeros(xs.shape)
+                    may.mesh(stxs, stys, real_zs, scalars=arr*-1, colormap='RdYlBu')
+
+            if plot_criticals:
+                crit_sets = [tracer.crits for tracer in self.tracers]
+                for side, crit_set in enumerate(crit_sets):
+                    maxima, minima, saddles, degenerate = crit_set
+                    maxima = crits_to_stereographic_projection(
+                        side, shape, maxima)
+                    print 'maxima are', maxima
+
+                    if len(maxima) > 0:
+                        scalars = 1. * n.ones(len(maxima))
+                        may.points3d(maxima[:, 0], maxima[:, 1],
+                                     n.zeros(len(maxima)), scalars,
+                                     color=(1, 0, 0))
+                    # if len(minima) > 0:
+                    #     ax.scatter(minima[:, 0], minima[:, 1], 60, c='b')
+                    # if len(saddles) > 0:
+                    #     ax.scatter(saddles[:, 0], saddles[:, 1], 60, color='yellow')
+
+        else:  # not stereographic, plot full sphere
+            for side, arr in enumerate(arrs):
+                arr[0, 0] = arr_max
+                arr[0, 1] = -1*arr_max
+                xs, ys, zs = side_to_xs_ys_zs(side, shape)
+                stxs, stys, stzs = vcube_to_sphere(xs, ys, zs)
+                may.mesh(stxs, stys, stzs, scalars=arr*-1, colormap='RdYlBu')
+
+            if plot_criticals:
+                crit_sets = [tracer.crits for tracer in self.tracers]
+                for side, crit_set in enumerate(crit_sets):
+                    maxima, minima, saddles, degenerate = crit_set
+                    print 'maxima are', maxima
+                    maxima = crits_to_sphere(side, shape, maxima)
+                    print 'maxima are', maxima
+
+                    if len(maxima[0]) > 0:
+                        print 'plotting'
+                        may.points3d(maxima[0], maxima[1], maxima[2],
+                                     color=(1, 0, 0))
+            
+
+    def plot_net(self, plot_criticals=True, trace_lines=False, cmap='RdYlBu',
+                 normalise=True):
         '''Plot component tracers via the cube net.'''
+        if plot_criticals:
+            self.find_critical_points()
         if trace_lines:
             self.trace_lines()
         
-        arrs = []
-        for tracer in self.tracers:
-            arrs.append(n.rot90(tracer.arr[::-1], 3))
+        arrs = [n.rot90(tracer.arr[::-1], 3) for tracer in self.tracers]
 
         shape = self.shape
 
@@ -120,39 +188,97 @@ class NeumannCubeHandler(object):
         full_arr[2*shape:(2*shape+shape), shape:(shape+shape)] = arrs[5]
 
         # y sides
-        full_arr[shape:(shape+shape), 0:(shape)] = arrs[2]
-        full_arr[shape:(shape+shape), 2*shape:(2*shape+shape)] = arrs[3]
+        full_arr[shape:(shape+shape), 0:(shape)] = arrs[3]
+        full_arr[shape:(shape+shape), 2*shape:(2*shape+shape)] = arrs[2]
 
-        lines = [[[shape, 0],
-                  [shape, shape],
-                  [0, shape]],
-                 [[2*shape, 0],
-                  [2*shape, shape],
-                  [3*shape, shape]],
-                 [[0, 2*shape],
-                  [shape, 2*shape],
-                  [shape, 4*shape]],
-                 [[3*shape, 2*shape],
-                  [2*shape, 2*shape],
-                  [2*shape, 4*shape]]]
-        lines = [n.array(line) - 0.5 for line in lines]
-        lines = map(n.array, lines)
+
+        abs_arr = n.abs(full_arr)
+        if normalise:
+            full_arr[0, 0] = n.max(abs_arr)
+            full_arr[0, 1] = full_arr[0, 0] * -1
+
+        boundary_lines = [[[shape, 0],
+                          [shape, shape],
+                          [0, shape]],
+                         [[2*shape, 0],
+                          [2*shape, shape],
+                          [3*shape, shape]],
+                         [[0, 2*shape],
+                          [shape, 2*shape],
+                          [shape, 4*shape]],
+                         [[3*shape, 2*shape],
+                          [2*shape, 2*shape],
+                          [2*shape, 4*shape]]]
+        boundary_lines = [n.array(line) - 0.5 for line in boundary_lines]
 
         fig, ax = plt.subplots()
 
         ax.imshow(full_arr, interpolation='none',
-                  cmap='RdYlBu_r')
+                  cmap=cmap, alpha=1.0)
 
-        for line in lines:
+        if plot_criticals:
+            crit_sets = [tracer.crits for tracer in self.tracers]
+            for side, crit_set in enumerate(crit_sets):
+                maxima, minima, saddles, degenerate = crit_set
+                maxima = net_representation_shift(side, shape, maxima)
+                minima = net_representation_shift(side, shape, minima)
+                saddles = net_representation_shift(side, shape, saddles)
+
+                if len(maxima) > 0:
+                    ax.scatter(maxima[:, 0], maxima[:, 1], 60, c='r')
+                if len(minima) > 0:
+                    ax.scatter(minima[:, 0], minima[:, 1], 60, c='b')
+                if len(saddles) > 0:
+                    ax.scatter(saddles[:, 0], saddles[:, 1], 60, color='yellow')
+
+        for line in boundary_lines:
             ax.plot(line[:, 0], line[:, 1], color='black',
                     linewidth=2.0)
 
+        if trace_lines:
+            line_sets = [tracer.lines for tracer in self.tracers]
+            for side, lines in enumerate(line_sets):
+                print side
+                for line in lines:
+                    shifted_line = net_representation_shift(side, shape, line)
+                    ax.plot(shifted_line[:, 0], shifted_line[:, 1], '-',
+                            color='purple')
+                    
+        ax.set_xticks([])
+        ax.set_yticks([])
+
         ax.set_xlim(0, 3*shape-1)
         ax.set_ylim(0, 4*shape-1)
+
+        fig.tight_layout()
             
         return fig, ax
 
-    
+def net_representation_shift(side, shape, line):
+    '''Given a side, a shape, and a line from that side's array, shifts
+    the line in the x-y plane so that it will be in the right place in
+    a net diagram.
+    '''
+    line = n.array(line)
+    if side == 0:
+        line[:, 0] += shape 
+        line[:, 1] += shape
+    elif side == 1:
+        line[:, 0] += shape
+        line[:, 1] += 3*shape
+    elif side == 2:
+        line[:, 0] += 0
+        line[:, 1] += shape
+    elif side == 3:
+        line[:, 0] += 2*shape
+        line[:, 1] += shape
+    elif side == 4:
+        line[:, 0] += shape
+        line[:, 1] += 0
+    elif side == 5:
+        line[:, 0] += shape 
+        line[:, 1] += 2*shape 
+    return line
 
 def side_xy_to_xyz(side, x, y):
     '''Takes a cube side, and x,y values, and returns the x,y,z angles.'''
@@ -168,6 +294,7 @@ def side_xy_to_xyz(side, x, y):
         return x, 0.5, y
     elif side == 5:
         return x, -0.5, -y
+vside_xy_to_xyz = n.vectorize(side_xy_to_xyz)
 
 def cube_to_angles(x, y, z):
     '''Takes points on a face of the cube, and maps them to angles on the
@@ -177,11 +304,88 @@ def cube_to_angles(x, y, z):
     theta = n.arccos(z/r)
     return theta, phi
 
+def angles_to_sphere(theta, phi):
+    '''Takes angles on the 2-sphere, and gives their x,y positions on that
+    sphere.
+    '''
+    return n.sin(theta)*n.cos(phi), n.sin(theta)*n.sin(phi), n.cos(theta)
+
+
+
+
 def angles_to_stereographic_projection(theta, phi):
     '''Takes angles on the 2-sphere, and gives their x,y positions under
     stereographic projection from z=-1?'''
     chi = theta / 2.  # Projection from south pole
-    r = n.tan(chi)
-    x = r*n.sin(theta)*n.cos(phi)
-    y = r*n.sin(theta)*n.sin(phi)
+    x = 2*n.tan(chi)*n.cos(phi)
+    y = 2*n.tan(chi)*n.sin(phi)
     return x, y
+
+def crits_to_stereographic_projection(side, shape, crits):
+    scaled_crits = []
+    for crit in crits:
+        scaled_crits.append(n.array(crit)/float(shape) - 0.5)
+    print 'crits', crits
+    print 'scaled', scaled_crits
+    return(side_line_to_stereographic_projection(side, scaled_crits))
+
+def crits_to_sphere(side, shape, crits):
+    scaled_crits = []
+    for crit in crits:
+        scaled_crits.append(n.array(crit)/float(shape) - 0.5)
+    xyzs = []
+    for crit in scaled_crits:
+        xyzs.append(side_xy_to_xyz(side, crit[0], crit[1]))
+    xyzs = n.array(xyzs)
+    print 'xyzs are', xyzs
+
+    print 'xyzs are', xyzs
+    sphere_xyzs = vcube_to_sphere(xyzs[:, 0], xyzs[:, 1], xyzs[:, 2])
+    print 'and on sphere', sphere_xyzs
+    return sphere_xyzs
+
+def side_line_to_stereographic_projection(side, line):
+    points = []
+    for point in line:
+        points.append(side_xy_to_stereographic_projection(side, point[0], point[1]))
+    return n.array(points)
+
+def side_line_to_sphere(side, line):
+    points = []
+    for point in line:
+        points.append(side_xy_to_sphere(side, point[0], point[1]))
+    return n.array(points)
+
+def side_to_xs_ys_zs(side, shape):
+    xs, ys = n.mgrid[-0.5:0.5:shape*1j, -0.5:0.5:shape*1j]
+    zs = n.ones(xs.shape) * 0.5
+
+    if side == 0:
+        return xs, ys, zs
+    elif side == 1:
+        return xs, ys, -1*zs
+    elif side == 5:
+        return zs, ys, -1*xs
+    elif side == 4:
+        return -1*zs, ys, xs
+    elif side == 2:
+        return -1*xs, zs, -1*ys
+    elif side == 3:
+        return xs, -1*zs, ys
+
+    
+# def cube_lines(points=100):
+#     ls = []
+    
+
+def cube_to_stereographic_projection(x, y, z):
+    angles = cube_to_angles(x, y, z)
+    return angles_to_stereographic_projection(*angles)
+vcube_to_stereographic_projection = n.vectorize(
+    cube_to_stereographic_projection)
+
+def cube_to_sphere(x, y, z):
+    angles = cube_to_angles(x, y, z)
+    return angles_to_sphere(*angles)
+vcube_to_sphere = n.vectorize(cube_to_sphere)
+    
