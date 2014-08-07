fbfb330096c56570a404fb4f2a2eb8260dd89946
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-08-07 16:21:05 +0100

    Added numpy include to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import numpy
+
 ext_modules = [
         Extension("cneumann",["cneumann.pyx"], libraries=["m"]),
         Extension("ctwosphere",["ctwosphere.pyx"], libraries=["m"]),
@@ -11,5 +13,6 @@ ext_modules = [
 setup(
   name = 'cneumann',
   cmdclass = {'build_ext': build_ext},
+  include_dirs = [numpy.get_include()],
   ext_modules = ext_modules
 )
