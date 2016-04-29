144e288efdd01412cea6c2df90d60064173a5ac3
Alexander Taylor <alexanderjohntaylor@gmail.com>
2016-04-29 12:22:31 +0100

    Removed unnecessary build script

--- build_cneumann.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Builds the cyknot module
-
-# Tell it where the numpy header files are
-export CFLAGS=-I/usr/lib/python2.7/site-packages/numpy/core/include/ 
-
-python2 setup.py build_ext --inplace
