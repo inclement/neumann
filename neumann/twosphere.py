b71893b36fea90c6e5c07118b8e623a4300d81aa
Alexander Taylor <alexanderjohntaylor@gmail.com>
2016-09-26 13:05:20 +0100

    Added vispy plotting function

--- neumann/twosphere.py
+++ neumann/twosphere.py
@@ -82,6 +82,61 @@ def plot_func(func, modulation=0., shape=(101, 101), cmap='RdYlBu',
         surf.module_manager.scalar_lut_manager.lut.table = lut
         may.draw()
 
+def plot_func_vispy(func, modulation=0., shape=(101, 101), cmap='RdYlBu',
+                    emph_doms=False, clf=True, invert_modulation=True,
+                    stereographic=False, offset=(0, 0, 0)):
+    '''Takes a function and plots it over a sphere.
+
+    Modulation argument controls surface modulation
+    '''
+
+    import mayavi.mlab as may
+    if clf:
+        may.clf()
+
+    if not stereographic:
+        theta, phi = n.mgrid[0:n.pi:shape[0]*1j, 0:2*n.pi:shape[1]*1j]
+    else:
+        theta, phi = n.mgrid[0:0.93*n.pi:shape[0]*1j, 0:2*n.pi:shape[1]*1j]
+
+    func = n.vectorize(func)
+    s = func(theta, phi).real
+
+    maxs = n.max(s)
+    norms = s / maxs
+    if invert_modulation:
+        r = 1. - modulation*norms
+    else:
+        r = 1. + modulation * norms
+
+    if not stereographic:
+        x = r*n.sin(theta)*n.cos(phi)
+        y = r*n.sin(theta)*n.sin(phi)
+        z = r*n.cos(theta)
+    else:
+        x = 2*r*n.tan(theta/2.)*n.cos(phi)
+        y = 2*r*n.tan(theta/2.)*n.sin(phi)
+        z = n.zeros(theta.shape) + r
+        
+    x += offset[0]
+    y += offset[1]
+    z += offset[2]
+    print('xmin is', n.min(x))
+
+    surf = may.mesh(x, y, z, scalars=s, colormap=cmap)
+    if emph_doms:
+        surf = may.mesh(x, y, z, scalars=s, colormap=cmap)
+        lut = surf.module_manager.scalar_lut_manager.lut.table.to_array()
+        lut[128:, 0] = n.zeros(128)
+        lut[128:, 1] = n.zeros(128)
+        lut[128:, 2] = n.zeros(128)
+        lut[:128, 0] = n.ones(128)*255
+        lut[:128, 1] = n.ones(128)*255
+        lut[:128, 2] = n.ones(128)*255
+        print('lut is', lut)
+        surf.module_manager.scalar_lut_manager.lut.table = lut
+        may.draw()
+
     
 def plot_comparison(l, m, shape=(101, 101)):
 
